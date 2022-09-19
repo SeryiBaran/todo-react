221f0ba16573725511fc98c4a799e63479d9c361
Ivan Muzyka <seryibaranminepy@yandex.ru>
2022-09-19 19:14:04 +0300

    Update index.jsx

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -3,7 +3,7 @@ import { useStore } from "effector-react";
 
 import { generateKey } from '@/utils';
 
-import { $todos, addTodo } from '@/state/TodosState';
+import { $todos } from '@/state/TodosState';
 
 import { TodoItem } from './TodoItem';
 
