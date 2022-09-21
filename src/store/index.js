a2e560fdcac01139e09c056be664148cfc830709
SeryiBaran <seryibaranminepy@yandex.ru>
2022-09-21 17:50:29 +0300

    Naming

--- src/store/index.js
+++ src/store/index.js
@@ -3,31 +3,39 @@ import { persist } from 'effector-storage/local';
 
 import { generateKey } from '@/utils';
 
-export const defaultValue = [];
+export const defaultTodos = [];
 
-export const $todos = createStore(defaultValue);
+export const $todos = createStore(defaultTodos);
 
 persist({ store: $todos, key: 'todos' });
 
-export const todoAddedWithId = createEvent();
-export const todoRemoved = createEvent();
-export const todoEdited = createEvent();
-
-$todos.on(todoAddedWithId, (state, todo) => [...state, todo]);
-$todos.on(todoRemoved, (state, id) => {
+const onTodoAddedWithId = (state, todo) => [...state, todo];
+const onTodoRemoved = (state, id) => {
   const copy = [...state];
   const index = copy.findIndex(todo => todo.id === id);
   copy.splice(index, 1);
   return copy;
-});
-$todos.on(todoEdited, (state, { id, newContent }) => {
+};
+const onTodoEdited = (state, { id, newContent }) => {
   const copy = [...state];
   const index = copy.findIndex(todo => todo.id === id);
   copy[index].content = newContent;
   return copy;
-});
-
-export const todoAdded = todoAddedWithId.prepend(content => ({
+};
+const prependTodoAddedWithId = content => ({
   id: generateKey(),
   content,
-}));
+});
+
+export const todoAddedWithId = createEvent();
+export const todoRemoved = createEvent();
+export const todoEdited = createEvent();
+export const resetted = createEvent();
+
+export const todoAdded = todoAddedWithId.prepend(prependTodoAddedWithId);
+
+$todos
+  .on(todoAddedWithId, onTodoAddedWithId)
+  .on(todoRemoved, onTodoRemoved)
+  .on(todoEdited, onTodoEdited)
+  .reset(resetted)
