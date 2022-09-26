8647caa5c16ea54d4b0f55dffdf798e37f461b94
SeryiBaran <seryibaranminepy@yandex.ru>
2022-09-26 21:17:25 +0300

    UUID (для стандарта)

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
-import { nanoid } from 'nanoid';
+import { v4 as uuidv4 } from 'uuid';
 
 // Проверка валидности содержимого для контента в todo
 export const todoContentIsValid = content => !!content.trim();
 
-// Генерация рандомного ID
-export const generateId = () => nanoid();
+// Генерация ID (для ToDo и react key)
+export const generateId = () => uuidv4();
 
 // Поиск индекса todo в массиве по значению id
 export const searchTodoById = (todoArray, id) => todoArray.findIndex(todo => todo.id === id);
