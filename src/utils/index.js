b8ce1ebc5afb5d146a16df20c0282260def025b2
SeryiBaran <seryibaranminepy@yandex.ru>
2022-09-26 20:50:39 +0300

    Fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,10 @@
+import { nanoid } from 'nanoid';
+
 // Проверка валидности содержимого для контента в todo
 export const todoContentIsValid = content => !!content.trim();
 
-// Генерация рандомного ID, в формате "prefix_рандомнаяСтрокаИзNanoID"
-export const generateId = () => crypto.randomUUID();
+// Генерация рандомного ID
+export const generateId = () => nanoid();
 
 // Поиск индекса todo в массиве по значению id
 export const searchTodoById = (todoArray, id) => todoArray.findIndex(todo => todo.id === id);
