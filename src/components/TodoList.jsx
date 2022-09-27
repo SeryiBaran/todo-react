c5b254fd40bd11dcb39dfa6e1ac6cbcc5328b986
SeryiBaran <seryibaranminepy@yandex.ru>
2022-09-27 13:07:03 +0300

    Fix

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import { useStore } from 'effector-react';
 
-import { generateId } from '@/utils';
-
 import { $todos } from '@/store';
 
 import { TodoItem } from './TodoItem';
@@ -22,7 +20,7 @@ export const TodoList = () => {
       {/* Использован rest оператор, дабы не мутировать состояние */}
       {/* Использован метод .reverse, чтобы тудушки шли снизу вверх */}
       {[...todos].reverse().map(todo => (
-        <TodoItem key={generateId()} todoId={todo.id} content={todo.content} />
+        <TodoItem key={todo.id} todoId={todo.id} content={todo.content} />
       ))}
     </StyledTodoList>
   );
